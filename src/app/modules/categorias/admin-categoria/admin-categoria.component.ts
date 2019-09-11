452e0fc188be54b2604c37a4e410d9224901bd57
Manuel <manuelmedina012@gmail.com>
2019-09-11 17:59:02 -0400

    categoria actualizado 11/09

---
+++ src/app/modules/categorias/admin-categoria/admin-categoria.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-categoria',
+  templateUrl: './admin-categoria.component.html',
+  styleUrls: ['./admin-categoria.component.css']
+})
+export class AdminCategoriaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
