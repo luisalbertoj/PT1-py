e05ee6730348840d6e2d869c3f9489695839f0e9
enzounzais2 <enzo.unza@gmail.com>
2019-09-18 12:01:28 -0400

    modulos pacientes 2

--- src/app/modules/pacientes/registro-pacientes/registro-pacientes.component.ts
+++ src/app/modules/pacientes/registro-pacientes/registro-pacientes.component.ts
@@ -47,6 +47,7 @@ export class RegistroPacienteComponent  {
           'Peticion exitosa',
           'success'
         );
+        this.cargar(this.paginacion);
       },
       (error: any) => {
         console.log(error);
